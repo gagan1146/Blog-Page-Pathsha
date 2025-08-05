7faca8a8786593e98867a9b006ae4cd5fe753633
atharvmittal <atharv.22b0101030@abes.ac.in>
2025-08-05 18:26:58 +0530

    added css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
 import BlogsPage from './components/BlogsPage/BlogsPage'
-import Footer from './components/Footer/Footer'
+import Footer from './components/CourseType/Footer/Footer'
 import CoursesSection from './components/CoursesSection/CoursesSection'
 import NewsletterSection from './components/NewsletterSection/NewsletterSection'
 import OtherBlogs from './components/OtherBlogs/OtherBlogs'
